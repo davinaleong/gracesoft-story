ed941b6d4f0766336149a6c23bf199bc8971e182
Davina Leong <leong.shi.yun@gmail.com>
2026-04-08 12:41:28 +0800

    Updated the website icon.

--- resources/views/story/chapter.blade.php
+++ resources/views/story/chapter.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Chapter {{ substr($commit->sha, 0, 7) }}</title>
+    <link rel="icon" type="image/svg+xml" href="{{ asset('logo.svg') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="gs-page">
--- resources/views/story/timeline.blade.php
+++ resources/views/story/timeline.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $repository->full_name ?: $repository->name }} Story</title>
+    <link rel="icon" type="image/svg+xml" href="{{ asset('logo.svg') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="gs-page">
